ce1ca4d56ba73b12e73f7f3a3fec46fb810f5af7
JUNGHOON AN <gnstm831@naver.com>
2024-10-15 16:19:12 +0900

    활용2-섹션2. API 개발 기본
    2. 회원 수정 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,11 +3,10 @@ package jpabook.jpashop.api;
 import jakarta.validation.Valid;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,15 +20,6 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
-    @Data
-    static class CreateMemberResponse {
-        private Long id;
-
-        public CreateMemberResponse(Long id) {
-            this.id = id;
-        }
-    }
-
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
 
@@ -40,9 +30,41 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+
+        // 수정할 땐 가급적 변경감지를 사용할 것
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
 
     @Data
     static class CreateMemberRequest {
         private String name;
     }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
 }
--- jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -17,7 +17,7 @@ public class MemberService {
 
     /*
             회원 가입
-         */
+    */
     @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
@@ -42,5 +42,18 @@ public class MemberService {
 
     public Member findOne(Long memberId) {
         return memberRepository.findOne(memberId);
+    }
+
+    @Transactional
+    public void update(Long id, String name) {
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+        /*
+            영속상태의 Member를 setName으로 이름을 바꿔주면
+            메서드가 종료될 때, Spring AOP가 @Transactional 애너테이션에 의해 트랜잭션을 커밋한다.
+            트랜잭션 커밋 시점에 JPA는 자동으로 flush()를 호출하여, 영속성 컨텍스트에 있는 변경된 엔티티를 데이터베이스에 반영한다.
+            이때 변경된 엔티티에 대한 SQL 쿼리가 실행된다.
+            트랜잭션 커밋이 완료되면, 데이터베이스에 최종적으로 변경 사항이 commit()을 통해 적용된다.
+         */
     }
 }
